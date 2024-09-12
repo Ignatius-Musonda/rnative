3f54ac0492d0a4f38fc450e0ffbb798632432d86
Ignatius Musonda <musondaignatius@gmail.com>
2024-09-12 18:44:34 +0200

    folder setup

---
+++ app/(auth)/SIgnup.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SIgnup = () => {
+  return (
+    <View>
+      <Text>SIgnup</Text>
+    </View>
+  )
+}
+
+export default SIgnup
+
+const styles = StyleSheet.create({})
